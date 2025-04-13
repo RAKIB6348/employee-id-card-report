f5be5f632910b53d0fa93d239f8ec00e12e93c28
RAKIB6348 <shahidulhuda0@gmail.com>
2025-04-13 10:43:42 +0600

    add image in the id card

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Employee ID Card Report",
+    "version": "17.0.1.0.0",
+    "category": "Human Resources",
+    "summary": "Print Employee ID Cards",
+    'license':'LGPL-3',
+    "depends": ["hr"],
+    "data": [
+        "views/employee_id_card_menu.xml",
+        "report/employee_id_card.xml"
+    ],
+    "installable": True,
+    "application": True
+}
